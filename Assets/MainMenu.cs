605c5c8f1f112d3202b015604225ad3023f535f2
ArvinT <arvin288@hotmail.com>
2019-06-06 23:30:28 +0100

    Add Core Files

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Text scoreText;
+
+    public void Start()
+    {
+        scoreText.text = PlayerPrefs.GetInt("Score").ToString();
+    }
+    // Use this for initialization
+    public void ToGame ()
+    {
+        SceneManager.LoadScene("Game");	
+	}
+	
+}
